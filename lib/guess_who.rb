615167cf4c10c9de4423844b654a978cb58c383d
F-Hameau <f.hameau@gmail.com>
2019-10-17 15:17:15 +0200

    first chore methids

--- lib/guess_who.rb
+++ lib/guess_who.rb
@@ -2,5 +2,26 @@ require "guess_who/version"
 
 module GuessWho
   class Error < StandardError; end
-  # Your code goes here...
+  class Session
+    def initialize(name_and_photo)
+      @name_and_photo = name_and_photo
+      @score = 0
+    end
+
+    def display_photo
+      name, @photo = @name_and_photo.first
+      photos = @name_and_photo.values
+      [name, photos]
+    end
+
+    def good_answer?(photo)
+      (photo == @photo).tap do |res|
+        @score += 100 if res
+      end
+    end
+
+    def score
+      @score
+    end
+  end
 end
--- spec/guess_who_spec.rb
+++ spec/guess_who_spec.rb
@@ -3,4 +3,21 @@ RSpec.describe GuessWho do
     expect(GuessWho::VERSION).not_to be nil
   end
 
+  let(:name_and_photo) do
+    {
+      'François Hameau' => '/teams/francois.png',
+      'David Ruyer' => '/teams/david.png',
+      'Renaud Pestre' => '/teams/renaud.png'
+    }
+  end
+
+  let(:session) do
+    GuessWho::Session.new(name_and_photo)
+  end
+
+  it 'runs the game' do
+    expect(session.display_photo).to eq ['François Hameau', ['/teams/francois.png', '/teams/david.png', '/teams/renaud.png']]
+    expect(session.good_answer?('/teams/francois.png')).to eq true
+    expect(session.score).to eq 100
+  end
 end
