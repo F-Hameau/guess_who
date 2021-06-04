9804e5eb61e2c46576e94ca2a05ad2ab9e1f0516
F-Hameau <f.hameau@gmail.com>
2021-06-04 16:37:05 +0200

    ok

--- api/test.rb
+++
@@ -1,9 +0,0 @@
-require "./load_data"
-require "sinatra/json"
-
-
-test =  JSON.parse(LoadData.get)["records"].map do
-  |rec| rec["fields"]
-end.to_h{ |r| [r["Name"],r["Lien Cloudinary"]]  }.compact
-
-print test
